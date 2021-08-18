9aedf9da598d3e8f0ff149752953c43e681088aa
Subifa071 <83801961+Subifa071@users.noreply.github.com>
2021-08-18 18:23:56 +0545

    Update agreement_file.py
    error fixed

--- rootPackage/agreement_file.py
+++ rootPackage/agreement_file.py
@@ -20,9 +20,10 @@ window_frame.place(x=0, y=0, relwidth=1, relheight=1)
 def agree():
     response = val.get()
     if response == "On":
-        continue_button = Button(window_frame, text="Continue", highlightbackground="yellow",
+        continue_button = Button(window_frame, text="Continue", bg="yellow",
                                  font=("Century, Gothic", 20), padx=9, pady=9)  # #FFFF00 = yellow color
         continue_button.grid(row=11, column=0)
+
     else:
         continue_button = Button(window_frame, text="Continue", bg="#FFFF00", font=("Century, Gothic", 20),
                                  state=DISABLED, padx=9, pady=9)  # #FFFF00 = yellow color
@@ -33,10 +34,16 @@ terms_and_condition_label = Label(window_frame, text="Terms and Conditions")
 terms_and_condition_label['font'] = myFont
 terms_and_condition_label.grid(row=0, column=0)
 
-with open('terms_and_conditions.txt', 'r') as f:
+with open('terms_and_conditions.txt', encoding='utf8') as f:
     lines = f.read()
 terms_label = Label(window_frame, text=lines)
 terms_label.grid(row=1, column=0)
+
+lines.split("######")
+with open('terms_and_conditions.txt', encoding='utf8') as f:
+    lines = f.read()
+terms_label = Label(window_frame, text=lines)
+terms_label.grid(row=9, column=10)
 
 lines.split("######")
 
@@ -44,7 +51,7 @@ sound_label = Label(window_frame, text="Sound and music")
 sound_label['font'] = myFont
 sound_label.grid(row=2, column=0)
 
-with open('sound_music.txt', 'r') as f:
+with open('sound_music.txt', encoding='utf8') as f:
     lines = f.read()
 terms_label = Label(window_frame, text=lines)
 terms_label.grid(row=3, column=0)
@@ -53,7 +60,7 @@ permission_label = Label(window_frame, text="Permission")
 permission_label['font'] = myFont
 permission_label.grid(row=4, column=0)
 
-with open('permission.txt', 'r') as f:
+with open('permission.txt', encoding='utf8') as f:
     lines = f.read()
 permission_label = Label(window_frame, text=lines)
 permission_label.grid(row=5, column=0)
@@ -61,13 +68,7 @@ permission_label.grid(row=5, column=0)
 changes_label = Label(window_frame, text="Changes and Update")
 changes_label['font'] = myFont
 changes_label.grid(row=6, column=0)
-changes_label = Label(window_frame, text="Changes and Update")
-changes_label['font'] = myFont
-changes_label.grid(row=12, column=0)
-changes_label = Label(window_frame, text="Changes and Update")
-changes_label['font'] = myFont
-changes_label.grid(row=15, column=0)
-with open('changes.txt', 'r') as f:
+with open('changes.txt', encoding='utf8') as f:
     lines = f.read()
 changes_label = Label(window_frame, text=lines)
 changes_label.grid(row=7, column=0)
